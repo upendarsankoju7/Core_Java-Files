ca1df1e136f1e4d96a95f205f25146b2ac89f1f9
upendarsankoju7 <upendarsankoju7@gmail.com>
2024-09-27 04:28:48 +0530

    Added Java Files

---
+++ My-Java/src/DataTypes/FristProgram.java
@@ -0,0 +1,16 @@
+package DataTypes;
+
+public class FristProgram {
+
+   public static void main(String args[])
+   {
+	   var a = 1;
+	   var b = true;
+	   var c = false;
+	   
+	System.out.println("Hello World") ;   
+	System.out.println(7 + 8);
+	
+   }	
+   
+}
---
+++ My-Java/src/DataTypes/Variables.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello");            
+	}
+
+}
